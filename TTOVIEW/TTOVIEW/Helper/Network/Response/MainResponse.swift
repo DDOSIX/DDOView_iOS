de547d531092b2914c5063d97e611b25de000f63
qbbang <qbbang@gmail.com>
2021-07-11 10:45:07 +0900

    우와아아아아아아ㅜ아ㅜ아앜

---
+++ TTOVIEW/TTOVIEW/Helper/Network/Response/MainResponse.swift
@@ -0,0 +1,8 @@
+//
+//  MainResponse.swift
+//  TTOVIEW
+//
+//  Created by 이규현 on 2021/07/11.
+//
+
+import Foundation
---
+++ TTOVIEW/TTOVIEW/Scenes/ReviewDetail/Cell/ReplyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ReplyTableViewCell.swift
+//  TTOVIEW
+//
+//  Created by 이규현 on 2021/07/11.
+//
+
+import UIKit
+
+class ReplyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
